c8d3f8077a1e280e6c0a08fc10531adccd49c425
taimoor954 <taimoormuhammad954@gmail.com>
2021-01-19 13:58:13 +0500

    axios working and changes in app.use(helmet) INDEX FILE

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,6 @@
 
 const login = async (email, password) => {
 
-    console.log(email, password)
   var a=   await axios({
     method : 'POST',
     url : 'http://127.0.0.1:8000/api/v1/users/login',
